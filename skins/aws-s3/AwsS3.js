05e8042efe591ac689e8c7c090ecaa9f3f145414
Guilherme Sena da Silva <guilhermesena888@yahoo.com.br>
2020-10-13 15:29:10 -0300

    Update AwsS3.js

--- skins/aws-s3/AwsS3.js
+++ skins/aws-s3/AwsS3.js
@@ -1,10 +1,10 @@
 const AWS = require('aws-sdk');
 const fs = require('fs');
 
-AWS.config.update({
-  accessKeyId: 'AKIAJCTBMYT4JIJ3PMVA',
-  secretAccessKey: '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'
-});
+//AWS.config.update({
+//  accessKeyId: '',
+ // secretAccessKey: ''
+//});
 
 const s3 = new AWS.S3();
 function uploadToS3(fileNameOld, fileName, callback) {
